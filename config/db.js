1e1b7ac6cfe01f5ed149ff551657a60bfe27fe42
Jandro <jandroponpal@gmail.com>
2022-01-05 13:46:02 +0100

    Se agrega mysql2 , sequelize y realizacion del primer modelo

---
+++ config/db.js
@@ -0,0 +1,18 @@
+const { Sequelize } = require("sequelize");
+
+const sequelize = new Sequelize("uptasknode", "root", "admin", {
+  host: "localhost",
+  dialect: "mysql",
+  port: "3306",
+  operatorsAliases: false,
+  define: {
+    timestamps: false,
+  },
+
+  pool: {
+    max: 5,
+    min: 0,
+    acquire: 30000,
+    idle: 10000,
+  },
+});
